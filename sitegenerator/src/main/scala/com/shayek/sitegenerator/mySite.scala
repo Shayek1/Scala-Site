836e97ef6ab8b4bb891703c701c847fcac2e7521
Shayek Ahmed <shayek.a@outlook.com>
2025-11-08 01:53:50 +0000

    add scaffolding of site

--- sitegenerator/src/main/scala/com/shayek/sitegenerator/mySite.scala
+++ sitegenerator/src/main/scala/com/shayek/sitegenerator/mySite.scala
@@ -1,7 +1,29 @@
 package com.shayek.sitegenerator
 
+import os.*
+import scalatags.Text.all.*
+
 object mySite {
+  def blogRender() ={
+    //using os.pwd to anchor the relative path, thus making it the absolute path
+    val resourcePath = os.pwd/"sitegenerator"/"src"/"main"/"resources"
+    val outPath = resourcePath / "blogOut"
+    val blogRoot = resourcePath / "articles"
+
+    os.write(
+      outPath / "index.html",
+      html(
+        head(
+          //TODO add a css file here
+        ),
+        body(
+          h1("This is my site"),
+          //TODO add links
+        )
+      )
+    )
+  }
   def main(args: Array[String]): Unit = {
-    
+    blogRender()
   }
 }
